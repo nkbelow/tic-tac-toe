70b9bae6012df139a1021964922994561f2893c0
NicholasBelow <nkbelow@gmail.com>
2017-05-01 15:27:46 -0700

    add to board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,20 @@
+
+//board to create for winning algorithm
+var row1 = $('.square').slice(0, 3);
+var row2 = $('.square').slice(3, 6);
+var row3 = $('.square').slice(6, 9);
+var board = [row1, row2, row3];
+
+var checkWinner = function(arrays) {
+  //create function to check winner
+};
+
+
+
 $(document).ready(function() {
   var count = 1;
   $('.square').click(function() {
+    if(count < 9) {
     if (count % 2 === 1) {
       $(this).text('X');
       count++;
@@ -8,8 +22,10 @@ $(document).ready(function() {
       $(this).text('O');
       count++;
     }
+  }
   });
   $('button').click(function() {
+    count = 1;
     $('.square').empty();
   });
 });
